0b1b972cd7e72624bc31ca7ef63e8c0e61101d81
SakuraBlossom <shawnlum123@gmail.com>
2019-11-02 02:39:00 +0800

    Prevents numeric characters in name

--- src/main/java/seedu/address/model/person/parameters/Name.java
+++ src/main/java/seedu/address/model/person/parameters/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain characters (no digits) and spaces, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    private static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    private static final String VALIDATION_REGEX = "[\\p{Alpha}][\\p{Alpha} ]*";
 
     private final String fullName;
 
